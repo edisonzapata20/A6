8107831e66704628e243fe9499d56f45ab17a48f
Andres <andres.perez14@upr.edu>
2019-12-11 16:12:48 -0400

    Parser to client server run

--- src/parser.py
+++ src/parser.py
@@ -1,38 +1,46 @@
 import ply.yacc as yacc
 import ClientServer
 import lexer
+import mserver
+import mclient
+import yanise
+
 tokens = lexer.tokens
 
 from lexer import help
 
 a = []
 
-def p_ServerAssign_Connection(p):
 
+def p_ServerAssign_Connection(p):
     """ServerAssign_Connection : CONNECT SPACE NAME
                     | DISCONNECT SPACE NAME
     """
+
     if p[1] == "Connect":
         a.append(p[3])
-        print("Server" + " " + p[3] + " " +  "running")
+        print("Server" + " " + p[3] + " " + "running")
+        yanise.main()
     if p[1] == "Disconnect" and len(a) != 0 and a[0] == p[3]:
         a.remove(a[0])
-        print("Server" + " " + p[3] + " " +  "disconnected")
+        print("Server" + " " + p[3] + " " + "disconnected")
+    elif p[1] == "Disconnect" and len(a) == 0:
+        print("There is no running server.")
+
 
 def p_error(p):
-    print("Syntax error!")
+    print("Syntax error! Command should be 'Connect/Disconnect servername' ")
+
 
 parser = yacc.yacc()
 
 while True:
     try:
-        s = input('ServerCommunication>')
+        s = input("ServerCommunication (Type 'help' for assistance)>")
     except EOFError:
         break
     if not s: continue
-    if (s == 'help'):
+    if s == 'help':
         help()
         continue
     result = parser.parse(s)
-
-
--- src/server.py
+++ src/server.py
@@ -6,7 +6,7 @@ HEADERSIZE = 10
 # Define socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind of socket with IP and port
-s.bind((socket.gethostname(), 7777))
+s.bind((socket.gethostname(), 7777))  # Socket -> endpoint that recieves data
 # Prepares for message
 s.listen(5)
 
@@ -25,8 +25,8 @@ while True:
 
     clientsocket.send(bytes(msg, "utf-8"))
 
-    while True:
-        time.sleep(3)
-        msg = f"The time is: {time.time()}"
-        msg = f'{len(msg) :< {HEADERSIZE}}' + msg
-        clientsocket.send(bytes(msg, "utf-8"))
+    # while True:
+    #     time.sleep(3)
+    #     msg = f"The time is: {time.time()}"
+    #     msg = f'{len(msg) :< {HEADERSIZE}}' + msg
+    #     clientsocket.send(bytes(msg, "utf-8"))
